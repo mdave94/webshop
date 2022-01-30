ba8d39a886c957e482997a8ed0bba48aa6d68d80
mdave94 <molnardavid2002@gmail.com>
2022-01-30 19:23:55 +0100

    Categories.jsx added

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react'
+import Categories from '../components/home/Categories'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 
 export class HomePage extends Component {
   render() {
     return (
         <Fragment>
-           <FeaturedProducts />
+          <FeaturedProducts />
+          <Categories/>
         </Fragment>
     )
   }
