4e60a041e8f66372346433de97c067c211da0cf7
mdave94 <molnardavid2002@gmail.com>
2022-02-02 15:37:32 +0100

    Footer done

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -8,6 +8,7 @@ import FeaturedProducts from '../components/home/FeaturedProducts'
 import HomeTop from '../components/home/HomeTop'
 import HomeTopMobile from '../components/home/HomeTopMobile'
 import NewArrival from '../components/home/NewArrival'
+import FooterMobile from '../components/common/FooterMobile'
 
 export class HomePage extends Component {
      render() { 
@@ -27,8 +28,15 @@ export class HomePage extends Component {
                     <NewArrival />
                     <Categories />
                     <Collection />
-                    <FooterDesktop/>
-                    
+                   
+
+                    <div className="Desktop">
+                     <FooterDesktop/>
+                    </div>
+
+                    <div className="Mobile">
+                       <FooterMobile/>
+                    </div>
                </Fragment>
           )
      }
