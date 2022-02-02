03e47b95b06e9add723ad8f581978760355349e5
mdave94 <molnardavid2002@gmail.com>
2022-02-02 15:26:45 +0100

    Topmenu and Homepage modofied. Footer added

--- user/ecom/src/components/home/HomeTop.jsx
+++ user/ecom/src/components/home/HomeTop.jsx
@@ -15,7 +15,7 @@ class HomeTop extends Component {
                       <MegaMenu/>
                     </Col>
 
-                    <Col lg={6} md={6} sm={12} xs={12}>
+                    <Col lg={8} md={6} sm={12} xs={12}>
                     <HomeSlider/>
                     </Col>
                 </Row>
--- user/ecom/src/components/home/MegaMenu.jsx
+++ user/ecom/src/components/home/MegaMenu.jsx
@@ -14,7 +14,6 @@ class MegaMenu extends Component {
     MegaMenu(){
         var accordion = document.getElementsByClassName('accordion'); 
 
-        console.log(typeof(accordion));
         var accNum = accordion.length;
 
         for (var i = 0 ;i<accNum;++i){
@@ -108,6 +107,136 @@ class MegaMenu extends Component {
                 </div>
 
 
+                <div className='accordionMenuDivInside'>
+                    <button className='accordion'>
+                            <img className='accordionMenuIcon' alt='' src='https://www.flaticon.com/svg/vstatic/svg/3917/3917317.svg?token=exp=1643635736~hmac=b68722abae8a996d84c0f5bd77939e35' />
+                            &nbsp; Men's clothing
+                    </button>
+                    
+                    <div className='panel'>
+                        <ul>
+                            <li>
+                                <a href="x" className='accordionItem'>T-shirts</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Leggings</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Shorts</a>    
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='accordionMenuDivInside'>
+                    <button className='accordion'>
+                            <img className='accordionMenuIcon' alt='' src='https://www.flaticon.com/svg/vstatic/svg/3917/3917317.svg?token=exp=1643635736~hmac=b68722abae8a996d84c0f5bd77939e35' />
+                            &nbsp; Men's clothing
+                    </button>
+                    
+                    <div className='panel'>
+                        <ul>
+                            <li>
+                                <a href="x" className='accordionItem'>T-shirts</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Leggings</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Shorts</a>    
+                            </li>
+                        </ul>
+                    </div>
+                </div> <div className='accordionMenuDivInside'>
+                    <button className='accordion'>
+                            <img className='accordionMenuIcon' alt='' src='https://www.flaticon.com/svg/vstatic/svg/3917/3917317.svg?token=exp=1643635736~hmac=b68722abae8a996d84c0f5bd77939e35' />
+                            &nbsp; Men's clothing
+                    </button>
+                    
+                    <div className='panel'>
+                        <ul>
+                            <li>
+                                <a href="x" className='accordionItem'>T-shirts</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Leggings</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Shorts</a>    
+                            </li>
+                        </ul>
+                    </div>
+                </div> <div className='accordionMenuDivInside'>
+                    <button className='accordion'>
+                            <img className='accordionMenuIcon' alt='' src='https://www.flaticon.com/svg/vstatic/svg/3917/3917317.svg?token=exp=1643635736~hmac=b68722abae8a996d84c0f5bd77939e35' />
+                            &nbsp; Men's clothing
+                    </button>
+                    
+                    <div className='panel'>
+                        <ul>
+                            <li>
+                                <a href="x" className='accordionItem'>T-shirts</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Leggings</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Shorts</a>    
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                 <div className='accordionMenuDivInside'>
+                    <button className='accordion'>
+                            <img className='accordionMenuIcon' alt='' src='https://www.flaticon.com/svg/vstatic/svg/3917/3917317.svg?token=exp=1643635736~hmac=b68722abae8a996d84c0f5bd77939e35' />
+                            &nbsp; Men's clothing
+                    </button>
+                    
+                    <div className='panel'>
+                        <ul>
+                            <li>
+                                <a href="x" className='accordionItem'>T-shirts</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Leggings</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Shorts</a>    
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='accordionMenuDivInside'>
+                    <button className='accordion'>
+                            <img className='accordionMenuIcon' alt='' src='https://www.flaticon.com/svg/vstatic/svg/3917/3917317.svg?token=exp=1643635736~hmac=b68722abae8a996d84c0f5bd77939e35' />
+                            &nbsp; Men's clothing
+                    </button>
+                    
+                    <div className='panel'>
+                        <ul>
+                            <li>
+                                <a href="x" className='accordionItem'>T-shirts</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Leggings</a>    
+                            </li>
+
+                            <li>
+                                <a href="x" className='accordionItem'>Shorts</a>    
+                            </li>
+                        </ul>
+                    </div>
+                </div>
                 <div className='accordionMenuDivInside'>
                     <button className='accordion'>
                             <img className='accordionMenuIcon' alt='' src='https://www.flaticon.com/svg/vstatic/svg/3917/3917317.svg?token=exp=1643635736~hmac=b68722abae8a996d84c0f5bd77939e35' />
