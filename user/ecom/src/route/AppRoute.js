d5e0623b03d2c6d11833da033315de4790a50e14
mdave94 <molnardavid2002@gmail.com>
2022-02-03 12:54:05 +0100

    user Login Page with component added

--- user/ecom/src/route/AppRoute.js
+++ user/ecom/src/route/AppRoute.js
@@ -2,18 +2,19 @@ import React, { Component,Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import TestPage from '../pages/TestPage';
+import UserLoginPage from '../pages/UserLoginPage';
 
 class AppRoute extends Component {
   render() {
     return (
         <Fragment>
             <Switch>
-            <Route  exact path="/" >
-                    <HomePage/>
-              </Route>
-                <Route exact path="/test" >
-                    <TestPage/>
-                </Route>
+            <Route  exact path="/" component={HomePage}/>
+                    
+              
+
+            <Route exact path="/login" component={UserLoginPage} />
+               
             </Switch>
 
         </Fragment>
