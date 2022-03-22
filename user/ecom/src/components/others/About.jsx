ca18b5cc21a108810d423d0da487570b1027e1c3
mdave94 <molnardavid2002@gmail.com>
2022-03-22 13:12:21 +0100

    About.jsx, Purchase.jsx updated

--- user/ecom/src/components/others/About.jsx
+++ user/ecom/src/components/others/About.jsx
@@ -1,7 +1,30 @@
 import React, { Component, Fragment } from 'react'
 import { Container,Row,Col, Form,Button } from 'react-bootstrap'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
 
  class About extends Component {
+    constructor(){
+        super();
+        this.state={
+             about:""
+        }
+   }
+
+   componentDidMount(){
+        axios.get(AppURL.AllSiteInfo).then(response =>{
+             let StatusCode = response.status;
+             if(StatusCode==200){
+                  let JsonData = (response.data)[0]['about'];
+                  this.setState({about:JsonData});
+             } 
+
+        }).catch(error=>{
+
+        });
+   }
+
+
      render() {
           return (
                <Fragment>
@@ -10,18 +33,7 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
       <h4 className="section-title-login">About Me Page </h4>
       <p className="section-title-contact">
-    Hello! I'M David. I'm a web developer who start something new. 
-
-    I have some experiece with C++,Java,Javascript. In the last year I worked with Laravel framework, and now I started this project with Laravel backend and React frontend. <br></br>
-
-    For starting I use react-with-laravel-build-pwa-ecommerce-project Udemy course. I want to understand how to manage whole project with 2 framework, and I hope this course help whit it. <br></br>
-
-    After this project I going to make a new frontend design, making this project litlebit more mine :D. <br></br>
-
-    Peace ! 
-
-
-
+          {this.state.about}
       </p>
 
 
