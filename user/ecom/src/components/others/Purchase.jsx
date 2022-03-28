6ce34b1230837a967d61f297fd9a419dcbe49f79
mdave94 <molnardavid2002@gmail.com>
2022-03-28 11:14:42 +0200

    Purchase and redund page updated with placeholder loading screen

--- user/ecom/src/components/others/Purchase.jsx
+++ user/ecom/src/components/others/Purchase.jsx
@@ -1,76 +1,98 @@
-import React, { Component, Fragment } from 'react';
-import { Container,Row,Col, } from 'react-bootstrap';
+import React, { Component, Fragment } from 'react'
+import { Container,Row,Col, Form,Button } from 'react-bootstrap'
 import AppURL from '../../api/AppURL';
-import axios from 'axios';
+import axios from 'axios'
 import ReactHtmlParser from 'react-html-parser';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-export class Purchase extends Component {
-
+class Privacy extends Component {
      constructor(){
           super();
           this.state={
-               purchase:""
+               privacy:"",
+               loaderDiv:"",
+               mainDiv:"d-none"
           }
      }
 
      componentDidMount(){
-          
-          let SiteInfoPurchase = sessionStorage.getItem("AllSiteInfo");
-          console.log('SiteInfoPurchase');
-          console.log(SiteInfoPurchase);
-          if(SiteInfoPurchase === null){
+          axios.get(AppURL.AllSiteInfo).then(response =>{
+               let StatusCode = response.status;
+               if(StatusCode==200){
+                    let JsonData = (response.data)[0]['privacy'];
+                    this.setState({privacy:JsonData,loaderDiv:"d-none",mainDiv:""});
+               } 
 
-               axios.get(AppURL.AllSiteInfo).then(response =>{
-                    let StatusCode = response.status;
-                    let JsonData =(response.data)[0]['purchase_guide'];
-                  
-                    if(StatusCode === 200 && JsonData !== undefined){
-
-                         this.setState({purchase:JsonData});
-                         sessionStorage.setItem("SiteInfoPurchase",JsonData)
-                    } 
-                    else{ 
-
-                         toast.error("Somthing Went Wrong",{
-                              position: "bottom-center"
-                         });
-                    }
-      
-         
           }).catch(error=>{
-               toast.error("Somthing Went Wrong",{
-                    position: "bottom-center"
-               });
-          });
 
-     }  // end If Conditon 
-     else{
-          this.setState({purchase:SiteInfoPurchase});
+          });
      }
 
-     } 
 
-     render() {
+
+     render() { 
           return (
                <Fragment>
                <Container>
                     <Row className="p-2">
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-      <h4 className="section-title-login">Purchase Page </h4>
+
+
+            <div className={this.state.loaderDiv}>
+
+<div class="ph-item">
+<div class="ph-col-12">        
+<div class="ph-row">           
+<div class="ph-col-4"></div>
+<div class="ph-col-8 empty"></div>
+<div class="ph-col-6"></div>
+<div class="ph-col-6 empty"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+</div>
+</div>
+</div>
+
+
+<div class="ph-item">
+<div class="ph-col-12">        
+<div class="ph-row">           
+<div class="ph-col-4"></div>
+<div class="ph-col-8 empty"></div>
+<div class="ph-col-6"></div>
+<div class="ph-col-6 empty"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+</div>
+</div>
+</div>
+
+
+
+</div>
+
+ 
+
+
+<div className={this.state.mainDiv}>
+      <h4 className="section-title-login">Privacy Page</h4>
       <p className="section-title-contact">
-           { ReactHtmlParser(this.state.purchase) }
+
+      { ReactHtmlParser(this.state.privacy) }       
       </p>
 
-
+      </div>
+     
+     
                          </Col>
                     </Row>
                </Container>
-               <ToastContainer />
           </Fragment>
           )
      }
 }
 
-export default Purchase
+export default Privacy
