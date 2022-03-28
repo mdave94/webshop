2e63dc7c6b3a295fec6381510d9f05a6e4cb2ba3
mdave94 <molnardavid2002@gmail.com>
2022-03-28 14:18:48 +0200

    MegaMenuALl created but MegamenuMobile is uptaded

--- user/ecom/src/components/home/HomeTop.jsx
+++ user/ecom/src/components/home/HomeTop.jsx
@@ -2,9 +2,29 @@ import React, { Component, Fragment } from 'react';
 import { Container, Row,Col } from 'react-bootstrap';
 import MegaMenu from './MegaMenu';
 import HomeSlider from './HomeSlider';
+import AppURL from '../../api/AppURL';
+import axios from 'axios';
 
 
 class HomeTop extends Component {
+
+      constructor(){
+        super();
+        this.state ={
+            MenuData:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get(AppURL.AllCategoryDetails).then(response =>{ 
+              this.setState({MenuData:response.data});
+
+        }).catch(error=>{
+
+        });
+    }
+
+
   render() {
 
     return (
@@ -12,7 +32,7 @@ class HomeTop extends Component {
             <Container className='p-0 m-0 overflow-hidden' fluid={true}>
                 <Row>
                     <Col lg={3} md={3} sm={12} xs={12}>
-                      <MegaMenu/>
+                      <MegaMenu data={this.state.MenuData} />
                     </Col>
 
                     <Col lg={8} md={6} sm={12} xs={12}>
